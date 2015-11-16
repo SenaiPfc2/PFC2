515c3aa5615df10ee033eecf3f57c5804992e3c4
PFC2Senai <pfc2senai@gmail.com>
2015-11-16 09:55:47 -0200

    Teste git hub curso2

--- src/telas/Menu.java
+++ src/telas/Menu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package telas;
 
 /**
@@ -14,7 +9,8 @@ public class Menu extends javax.swing.JFrame {
     /**
      * Creates new form Menu
      */
-    public Menu() {      
+    public Menu() {
+        System.out.println("olá");
         initComponents();
     }
 
